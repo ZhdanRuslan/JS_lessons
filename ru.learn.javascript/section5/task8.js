8b157a1f16f38b548a0e3c4105ad53a5b00b1582
Ruslan <zhd.ruslan@gmail.com>
2017-04-01 15:40:29 +0300

    [Logger]
    Logger decorator with var args

---
+++ ru.learn.javascript/section5/task8.js
@@ -0,0 +1,28 @@
+/**
+ * Created by admin on 01.04.2017.
+ */
+
+function work(a, b) {
+    alert( a + b );
+}
+
+function makeLogging(f, log) {
+
+    function wrapper() {
+        log.push([].slice.call(arguments));
+        return f.apply(this, arguments);
+    }
+
+    return wrapper;
+}
+
+var log = [];
+work = makeLogging(work, log);
+
+work(1, 2);
+work(4, 5);
+
+for (var i = 0; i < log.length; i++) {
+    var args = log[i];
+    alert( 'Лог:' + args.join() );
+}
