573889b92402486f020cca7485638c53e6a15a3a
Ruslan <zhd.ruslan@gmail.com>
2017-04-02 10:39:43 +0300

    [OOP]
    Emulation of cofee machine

---
+++ ru.learn.javascript/section7/test1.js
@@ -0,0 +1,35 @@
+/**
+ * Created by admin on 02.04.2017.
+ */
+
+'use strict';
+function CoffeeMachine(power) {
+
+    this.waterAmount = 0;
+
+    var WATER_HEAT_CAPACITY = 4200;
+    var timerId;
+    var self = this;
+
+    function getBoilTime() {
+        return self.waterAmount * WATER_HEAT_CAPACITY * 80 / power;
+    }
+
+    function onReady() {
+        console.log( 'Кофе готов!' );
+    }
+
+    this.run = function() {
+        timerId = setTimeout(onReady, getBoilTime());
+    };
+
+    this.stop = function () {
+        clearTimeout(timerId)
+    };
+}
+
+var coffeeMachine = new CoffeeMachine(50000);
+coffeeMachine.waterAmount = 20;
+
+coffeeMachine.run();
+// coffeeMachine.stop();
