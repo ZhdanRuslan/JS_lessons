9dbfba243eee2eff9bf1788c78036dbe7e6bacd6
Ruslan <zhd.ruslan@gmail.com>
2017-04-02 13:07:43 +0300

    [OOP]
    ON/OFF cofee machine

---
+++ ru.learn.javascript/section7/test3.js
@@ -0,0 +1,63 @@
+/**
+ * Created by admin on 02.04.2017.
+ */
+
+/*
+В коде CoffeeMachine сделайте так, чтобы метод run выводил ошибку, если кофеварка выключена.
+
+    В итоге должен работать такой код:
+
+    var coffeeMachine = new CoffeeMachine(10000);
+    coffeeMachine.run(); // ошибка, кофеварка выключена!
+А вот так – всё в порядке:
+
+    var coffeeMachine = new CoffeeMachine(10000);
+    coffeeMachine.enable();
+    coffeeMachine.run(); // ...Кофе готов!
+*/
+
+function Machine(power) {
+    this._enabled = false;
+
+    var self = this;
+
+    this.enable = function() {
+        self._enabled = true;
+    };
+
+    this.disable = function() {
+        self._enabled = false;
+    };
+
+}
+
+function CoffeeMachine(power) {
+    Machine.apply(this, arguments);
+
+    var waterAmount = 0;
+
+    this.setWaterAmount = function(amount) {
+        waterAmount = amount;
+    };
+
+    var parentEnable = this.enable;
+    this.enable = function() {
+        parentEnable();
+        this.run();
+    }
+
+    function onReady() {
+        console.log( 'Кофе готов!' );
+    }
+
+    this.run = function() {
+        if (!this._enabled)
+            throw new Error("Кофеварка выключена");
+        setTimeout(onReady, 1000);
+    };
+
+}
+
+var coffeeMachine = new CoffeeMachine(10000);
+coffeeMachine.enable();
+coffeeMachine.run();
