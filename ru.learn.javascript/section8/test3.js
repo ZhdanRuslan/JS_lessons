0cc01e32db8197a7023b199f0e2346a2ff2094cb
Ruslan <zhd.ruslan@gmail.com>
2017-04-03 20:12:08 +0300

    [Prototype]
    Defer with args

---
+++ ru.learn.javascript/section8/test3.js
@@ -0,0 +1,20 @@
+/**
+ * Created by admin on 03.04.2017.
+ */
+
+Function.prototype.defer = function(ms) {
+    var f = this;
+    return function() {
+        var args = arguments,
+            context = this;
+        setTimeout(function() {
+            f.apply(context, args);
+        }, ms);
+    }
+}
+
+function f(a, b) {
+    alert( a + b );
+}
+
+f.defer(1000)(1, 2);
