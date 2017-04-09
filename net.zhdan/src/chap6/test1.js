675d924ebec43ae76b17985e27cdd77fd9b0b28c
Ruslan <zhd.ruslan@gmail.com>
2017-04-09 15:13:08 +0300

    [OOP]
    Object creation with new

---
+++ net.zhdan/src/chap6/test1.js
@@ -0,0 +1,14 @@
+function Ninja() {}
+
+Ninja.prototype.swingSword = function () {
+    return true;
+};
+
+/*Create with usual func invocation*/
+var ninja1 = Ninja();
+console.log(ninja1);
+
+/*Create obj with new*/
+var ninja2 = new Ninja();
+console.log(ninja2);
+
