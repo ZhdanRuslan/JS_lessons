81223f2ddc342911812af6072d79da45277f81e3
Ruslan <zhd.ruslan@gmail.com>
2017-04-09 22:31:52 +0300

    [OOP]
    Obj type

--- net.zhdan/src/chap6/test7.js
+++ net.zhdan/src/chap6/test7.js
@@ -1,13 +1,13 @@
 function Person() {}
 
 Person.prototype.dance = function () {};
-
 function Ninja() {}
 
 Ninja.prototype = {dance: Person.prototype.dance};
-
 var ninja = new Ninja();
 
 console.log(ninja instanceof Ninja);
 console.log(ninja instanceof Person);
 console.log(ninja instanceof Object);
+console.log({}.constructor);
+console.log([].constructor);
