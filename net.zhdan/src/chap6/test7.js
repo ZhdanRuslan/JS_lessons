374c983e674edf48482826dbde15d8789f6410d6
Ruslan <zhd.ruslan@gmail.com>
2017-04-09 21:51:48 +0300

    [OOP]
    instanceof

---
+++ net.zhdan/src/chap6/test7.js
@@ -0,0 +1,13 @@
+function Person() {}
+
+Person.prototype.dance = function () {};
+
+function Ninja() {}
+
+Ninja.prototype = {dance: Person.prototype.dance};
+
+var ninja = new Ninja();
+
+console.log(ninja instanceof Ninja);
+console.log(ninja instanceof Person);
+console.log(ninja instanceof Object);
