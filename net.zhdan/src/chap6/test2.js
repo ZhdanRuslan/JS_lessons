2da964db50d0b8a411870866e91ea1b932bc4016
Ruslan <zhd.ruslan@gmail.com>
2017-04-09 15:24:59 +0300

    [OOP]
    Method shadowing constructor <-> prototype

---
+++ net.zhdan/src/chap6/test2.js
@@ -0,0 +1,16 @@
+function Ninja() {
+
+    this.swung = false;
+
+    this.swingSword = function () {
+        return !this.swung;
+    };
+}
+
+Ninja.prototype.swingSword = function () {
+    return this.swung;
+};
+
+var ninja = new Ninja();
+
+console.log(ninja.swingSword());
