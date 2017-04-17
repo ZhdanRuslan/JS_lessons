3bc54c720ed31f15f372431738154f8912606aa4
Ruslan <zhd.ruslan@gmail.com>
2017-04-17 17:43:43 +0300

    [ex1]
    ready

--- net.zhdan/src/ex/ex1.js
+++ net.zhdan/src/ex/ex1.js
@@ -5,42 +5,28 @@
  Начальное значение по умолчанию равно 0. Генераторов можно создать сколько угодно.
  */
 
-/*
- var generator = sequence(10, 3);
- var generator2 = sequence(7, 1);
-
- console.log(generator()); // 10
- console.log(generator()); // 13
-
- console.log(generator2()); // 7
-
- console.log(generator()); // 16
-
- console.log(generator2()); // 8
- * */
-
 function sequence(start, step) {
 
     var count = start;
 
-    if (step === undefined){
+    if (!step) {
         step = 1;
     }
 
     return function () {
-
-        if (step) {
-            count = count + step;
-        } else {
-            count++;
-        }
-
+        count = count + step;
         return count;
     }
 }
 
 var generator = sequence(10, 3);
-// var generator2 = sequence(7, 1);
+var generator2 = sequence(7, 1);
 
-console.log(generator()); // 10
 console.log(generator()); // 13
+console.log(generator()); // 16
+
+console.log(generator2()); // 8
+
+console.log(generator()); // 19
+
+console.log(generator2()); // 9
